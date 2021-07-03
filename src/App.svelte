4fbf977cce5bb88a5ad6f7079ca7fb722630e2d5
Owusu kelvin Clark <kelvinclark2k77@gmail.com>
2021-07-03 21:56:05 +0100

    :building_construction: Write method for getting cookies

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,24 @@
 <script>
   import { route } from "./store/routing_store";
+  import { getCookies } from "./services/cookies";
   import Login from "./component/login.svelte";
   import SignUp from "./component/sign_up.svelte";
+
+  const cookies = getCookies();
+  if (cookies.id === "") {
+    route.set("login");
+  } else {
+    route.set("blog_list");
+  }
 </script>
 
 <main>
   {#if $route === "login"}
     <Login />
-  {:else if $route == "sign_up"}
+  {:else if $route === "sign_up"}
     <SignUp />
+  {:else if $route === "blog_list"}
+    <p>Blog List</p>
   {/if}
 </main>
 
--- src/store/routing_store.js
+++ src/store/routing_store.js
@@ -1,10 +1,10 @@
 import { writable } from "svelte/store";
 
-export const route = writable("login");
+export const route = writable("");
 // possible values
 /*
     login
     sign_up
-    blog list
+    blog_list
     edit_blog, read_blog, create_blog } one component
 */
