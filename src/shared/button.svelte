81176a337b5ce8d6ef235b3ff2b36f764c43f9c0
Owusu kelvin Clark <kelvinclark2k77@gmail.com>
2021-07-03 16:45:58 +0100

    :building_construction: Create a shared button component

---
+++ src/shared/button.svelte
@@ -0,0 +1,26 @@
+<script>
+  export let title = "No name";
+  export let flat = false;
+  export let primary = false;
+  export let secondary = false;
+  export let clickHandler = () => {};
+</script>
+
+<button
+  class="btn"
+  class:flat
+  class:primary
+  class:secondary
+  on:click={clickHandler}
+>
+  {title}
+</button>
+
+<style>
+  .btn {
+    padding: 0.3rem 1rem;
+    border: none;
+    background-color: rgb(111, 196, 111);
+    color: #fff;
+  }
+</style>
