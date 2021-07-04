ecd529acd7d9854df044fbc336883ddf50a96965
Owusu kelvin Clark <kelvinclark2k77@gmail.com>
2021-07-04 14:56:24 +0100

    :bento: Add cookies.js file

---
+++ src/services/cookies.js
@@ -0,0 +1,13 @@
+export function getCookies() {
+  let cookies = document.cookie
+    .split(";")
+    .map((cookie) => cookie.split("="))
+    .reduce(
+      (accumulator, [key, value]) => ({
+        ...accumulator,
+        [key.trim()]: decodeURIComponent(value),
+      }),
+      {}
+    );
+  return cookies;
+}
