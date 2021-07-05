c89f21a8b54606f94d97449fb4d089a5aba29b91
Owusu kelvin Clark <kelvinclark2k77@gmail.com>
2021-07-05 06:53:30 +0100

    :construction: Add blog item component

---
+++ src/component/blog_item.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let title;
+  export let body;
+  export let created_date;
+</script>
+
+<div class="blog-item">
+  <h3>{title}</h3>
+  <p>{body}</p>
+  <p>Created date: {created_date}</p>
+</div>
